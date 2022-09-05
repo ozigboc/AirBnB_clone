db489e49f4d0cef35a53726d66207ee9fd0dfdd2
codestronomer <johnrumide6@gmail.com>
2022-09-05 03:20:09 +0100

    [fix] test and bug fixes

--- console.py
+++ console.py
@@ -32,14 +32,14 @@ def arg_check(arg):
         print("** class name missing **")
         return None
     if args[0]:
-        if len(args) > 1:
-            if args[0] not in classes:
-                print("** class doesn't exist **")
-            else:
+        if args[0] not in classes:
+            print("** class doesn't exist **")
+        else:
+            if len(args) > 1:
                 key = f"{args[0]}.{args[1]}"
                 return key
-        else:
-            print("** instance id missing **")
+            else:
+                print("** instance id missing **")
     else:
         print("** class name missing **")
 
@@ -66,10 +66,11 @@ class HBNBCommand(cmd.Cmd):
         Creates a new instance of BaseModel Ex: $ create BaseModel
         """
         if arg:
-            if arg not in classes:
+            args = parse(arg)
+            if args[0] not in classes:
                 print("** class doesn't exist **")
             else:
-                new_model = classes[arg]()
+                new_model = classes[args[0]]()
                 new_model.save()
                 print(new_model.id)
         else:
@@ -77,9 +78,8 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, arg):
         """
-            Prints the string representation of an instance based\
+            Prints the string representation of an instance based
             on the class name and id.
-            Ex: $ show BaseModel 1234-1234-1234
         """
         key = arg_check(arg)
         if key is not None:
@@ -104,42 +104,40 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, arg):
         """
-        Prints all string representation of all instances\
-         based or not on the class name
+        Prints all string representation of all instances
+        based or not on the class name
         """
         str_list = []
-        for k, v in storage.all().items():
-            if arg:
-                if arg not in classes:
-                    print("** class doesn't exist")
-                    return False
+        if arg:
+            if arg not in classes:
+                print("** class doesn't exist **")
+                return False
+            for k, v in storage.all().items():
                 if arg == v.__class__.__name__:
                     str_list.append(str(v))
-            else:
+        else:
+            for k, v in storage.all().items():
                 str_list.append(str(v))
         print(str_list)
 
     def do_update(self, arg):
-        """updates an instance based on the classname and id\
-        by adding or updating attribute
-        Usage: update <class name> <id> <attribute name> "<attribute value>"
-        Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com"
-        """
+        """updates an instance based on the classname and id"""
         if arg:
             args = parse(arg)
         else:
             print("** class name missing **")
             return None
         if args[0]:
-            if len(args) > 1:
-                if args[0] not in classes:
-                    print("** class doesn't exist **")
-                else:
+            if args[0] not in classes:
+                print("** class doesn't exist **")
+            else:
+                if len(args) > 1:
                     key = f"{args[0]}.{args[1]}"
                     if key in storage.all():
                         if args[2]:
                             if args[3]:
-                                setattr(storage.all()[key], args[2], args[3])
+                                setattr(storage.all()[
+                                        key], args[2], args[3])
                                 storage.all().get(key).save()
                             else:
                                 print("** value missing **")
@@ -147,8 +145,8 @@ class HBNBCommand(cmd.Cmd):
                             print("** attribute name missing **")
                     else:
                         print("** no instance found **")
-            else:
-                print("** instance id missing **")
+                else:
+                    print("** instance id missing **")
         else:
             print("** class name missing **")
 
@@ -157,13 +155,15 @@ class HBNBCommand(cmd.Cmd):
         If this method is not overridden, it prints an error message and
         returns.
         """
+        new_list = []
         args = line.split('.')
         if args[0] in classes:
 
             if args[1] == "all()":
                 for k, v in storage.all().items():
                     if v.__class__.__name__ == args[0]:
-                        print(v)
+                        new_list.append(v)
+                print(new_list)
 
             elif args[1] == "count()":
                 count = 0
@@ -175,28 +175,31 @@ class HBNBCommand(cmd.Cmd):
             elif args[1].startswith("show"):
                 id_r = args[1][5:][:-1]
                 id = id_r.strip("\"")
-                print(id)
                 key = args[0] + "." + id
                 if key in storage.all():
                     print(storage.all().get(key))
                 else:
-                    print("No instance found")
+                    print("** no instance found **")
 
             elif args[1].startswith("destroy"):
                 id_r = args[1][8:][:-1]
                 id = id_r.strip("\"")
-                key = args[0] + "." + id
-                print(key)
-                if key in storage.all():
-                    storage.all().pop(key)
-                    storage.save()
+                if len(id) < 1:
+                    print("** instance id missing **")
                 else:
-                    print("No instance found")
+                    key = args[0] + "." + id
+                    if key in storage.all():
+                        storage.all().pop(key)
+                        storage.save()
+                    else:
+                        print("** no instance found **")
 
+            elif args[1].startswith("update"):
+                pass
             else:
                 print("** Invalid command **")
         else:
-            self.stdout.write('*** Unknown syntax: %s\n' % line)
+            print("** class doesn't exist **")
 
 
 if __name__ == '__main__':
