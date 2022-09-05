474b3919507db97bdfdd5e3709b7a71093596599
codestronomer <johnrumide6@gmail.com>
2022-09-05 03:23:36 +0100

    [fix] add args to User model

--- models/user.py
+++ models/user.py
@@ -5,7 +5,15 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represents a user model class"""
+    """
+    Represents a user model class
+
+    Attrs:
+        email: string - email of user
+        password: string - password of user
+        first_name: string - first_name of user
+        last_name: string - last_name of user
+    """
     email = ""
     password = ""
     first_name = ""
